4427e56255c7e6adb6091c239317c4cb4216fa28
johndoesstuff <johndoesstuff03@gmail.com>
2024-12-06 18:28:52 -0500

    added file banner

--- src/main.c
+++ src/main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ncurses.h>
+#include <string.h>
+
+int max(int a, int b) {
+	return a > b ? a : b;
+}
 
 int main(int argc, char *argv[]) {
 
@@ -14,6 +19,31 @@ int main(int argc, char *argv[]) {
 	
 	//initialize ncurses
 	initscr();
+	noecho();
+	cbreak();
+	int COMMAND_MODE = 0;
+	int EDIT_MODE = 1;
+	int REPLACE_MODE = 2;
+	curs_set(0);
+	int COLOR_BW = 1;
+	int colorMode = has_colors();
+	if (colorMode) {
+		start_color();
+		init_pair(COLOR_BW, COLOR_BLACK, COLOR_WHITE);
+	}
+
+	int screenWidth, screenHeight;
+	getmaxyx(stdscr, screenHeight, screenWidth);
+
+	//render file banner
+	attron(COLOR_PAIR(COLOR_BW));
+	int fileNameLen = strlen(targetFileName);
+	for (int i = 0; i < screenWidth; i++) {
+		char fillChar = colorMode ? ' ' : 183; //if color not supported use middle dot
+		mvaddch(screenHeight - 1, i, i < fileNameLen ? targetFileName[i] : fillChar);
+	}
+	attroff(COLOR_PAIR(COLOR_BW));
+
 	refresh();
 	getch();
 	endwin();
